87836e097a25571146989408624dd48a4d5aea76
Adrian Reeves <AdrianReevesmba@gmail.com>
2023-12-06 20:35:58 -0500

    Committed 3

--- scripts.js
+++ scripts.js
@@ -10,6 +10,8 @@ list.getElementsByTagName("li")[0].innerHTML = 'Fair Trade Coffee';
 
 // 3. Add an item 'Cheese Whiz'
 
+list.getElementsByTagName("li")[3].innerHTML = 'Cheese Whiz';
+
 // 4. Clear the list and programmatically add items from the array ['protein powder', 'muscle milk', 'power bars']
 
 // 5. Add the class 'important' to the muscle milk item.
