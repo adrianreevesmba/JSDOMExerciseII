549670d2a76f6c243214a0af8e87fbf54464ae7c
Adrian Reeves <AdrianReevesmba@gmail.com>
2023-12-07 16:34:45 -0500

    Adding 3

--- scripts.js
+++ scripts.js
@@ -6,12 +6,31 @@ list.getElementsByTagName("li")[0].innerHTML = 'Fair Trade Coffee';
 // 2. Remove 'Twinkies' from the list
 
  
-  list.removeChild(list.children[3];
+  list.removeChild(list.children[3]);
 
 // 3. Add an item 'Cheese Whiz'
+//Create a New element (li)
+//Set the Text of Element to Cheese Wiz
+//append element to the list
+const ul = document.querySelector('ul');
+const li = document.createElement('li');
 
-list.getElementsByTagName("li")[3].innerHTML = 'Cheese Whiz';
+ul.append(li); 
+li.innerText = "Cheese Whiz"
+
 
 // 4. Clear the list and programmatically add items from the array ['protein powder', 'muscle milk', 'power bars']
+// list.innerHTML = "";
+// let newItems = ['protein powder', 'muscle milk', 'power bars'];
+
+// newItems.map((a)=>{
+//Follow same steps in #3 above (lines 12-14)
+// }) 
+ 
+// list.getElementById("li").children[0, 1, 2, 3];
+// const element = document.getElementById("myList").children[0];
+// element.replaceChild(newItems, element.childNodes[0, 1, 2, 3]);
+
 
 // 5. Add the class 'important' to the muscle milk item.
+
